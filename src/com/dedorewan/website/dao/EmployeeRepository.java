4afc930451e106da3b7b69ac5b6926bbbf2bb7d4
dedorewan <rewandedo1009@gmail.com>
2016-04-01 17:47:31 +0700

    Group and Employee

--- src/com/dedorewan/website/dao/EmployeeRepository.java
+++ src/com/dedorewan/website/dao/EmployeeRepository.java
@@ -1,74 +1,65 @@
 package com.dedorewan.website.dao;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.dedorewan.website.dom.Employee;
 
 @Repository
-public class EmployeeRepository implements IEmployeeRepository {
+public class EmployeeRepository extends AbstractDao<Integer, Employee>
+		implements IEmployeeRepository {
 	@Autowired
 	private IGroupRepository groupRepository;
-	public List<Employee> generateList() {
-		List<Employee> eList = new ArrayList<Employee>();
-		String[] visas = { "THY", "NQN", "DED", "REW", "AKG", "THP", "YQN",
-				"EED", "WEW", "MKG" };
-		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-		Date date  = new Date();
-	    try {
-			date = sdf.parse("28.01.1993");
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		for (int i = 0; i < 5; i++) {
-			eList.add(new Employee(Long.valueOf(i), visas[i], "Nguyen Tien", "Thanh",
-					date, Long.valueOf(1212 + i)));
-			eList.add(new Employee(Long.valueOf(i+10), visas[i+5], "Thanh", "Nguyen",
-					date, Long.valueOf(1212 + i)));
-		}
-		return eList;
-	}
 
-	List<Employee> eList = generateList();
+	List<Employee> eList;
 
+	@SuppressWarnings("unchecked")
 	public List<Employee> findAll() {
+		Criteria criteria = createEntityCriteria();
+		eList = (List<Employee>) criteria;
 		return eList;
 	}
-	public boolean visaExsisted(String visa){
-		for(Employee e:eList){
-			if(e.getVisa().equals(visa)){
+
+	public boolean visaExsisted(String visa) {
+		eList = findAll();
+		for (Employee e : eList) {
+			if (e.getVisa().equals(visa)) {
 				return true;
 			}
 		}
 		return false;
 	}
-	public Long getEmployeeId(String visa){
+
+	public Long getEmployeeId(String visa) {
 		Long id = Long.valueOf(-1);
-		for(Employee e: eList){
-			if(e.getVisa().equals(visa)){
+		eList = findAll();
+		for (Employee e : eList) {
+			if (e.getVisa().equals(visa)) {
 				id = e.getId();
 			}
 		}
 		return id;
 	}
-	public String getEmployeeVisa(Long id){
-		for(Employee e: eList){
-			if(e.getId() == id){
+
+	public String getEmployeeVisa(Long id) {
+		eList = findAll();
+		for (Employee e : eList) {
+			if (e.getId() == id) {
 				return e.getVisa();
 			}
 		}
 		return null;
 	}
-	public List<Employee> availableEmployee(){
+
+	public List<Employee> availableEmployee() {
 		List<Employee> availableList = new ArrayList<Employee>();
-		for(Employee e: eList){
-			if (groupRepository.getGroupId(e.getId()) == -1){
+		eList = findAll();
+		for (Employee e : eList) {
+			if (groupRepository.getGroupId(e.getId()) == -1) {
 				availableList.add(e);
 			}
 		}
