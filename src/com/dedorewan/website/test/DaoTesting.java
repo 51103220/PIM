9da26cffd45d531e76ba0f0e58734aba5cf6ff59
dedorewan <rewandedo1009@gmail.com>
2016-04-05 13:22:29 +0700

    Added git test

--- src/com/dedorewan/website/test/DaoTesting.java
+++ src/com/dedorewan/website/test/DaoTesting.java
@@ -1,6 +1,8 @@
 package com.dedorewan.website.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+
 import org.hibernate.LockMode;
 import org.hibernate.LockOptions;
 import org.hibernate.Session;
@@ -17,12 +19,12 @@ import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.dedorewan.website.configuration.HibernateConfiguration;
 import com.dedorewan.website.configuration.PIMConfiguration;
-
 import com.dedorewan.website.dom.Project;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @TransactionConfiguration
-@ContextConfiguration(classes = { PIMConfiguration.class, HibernateConfiguration.class })
+@ContextConfiguration(classes = { PIMConfiguration.class,
+		HibernateConfiguration.class })
 @WebAppConfiguration
 public class DaoTesting {
 
@@ -42,10 +44,12 @@ public class DaoTesting {
 	@Test
 	public void testConcurrentUpdateProject() {
 		Session session = sessionFactory.openSession();
-		Project project = (Project) session.get(Project.class, Long.valueOf(23), new LockOptions(LockMode.OPTIMISTIC));
+		Project project = (Project) session.get(Project.class,
+				Long.valueOf(11), new LockOptions(LockMode.OPTIMISTIC));
 		try {
 			Transaction t = session.beginTransaction();
-			Project _project = (Project) session.get(Project.class, Long.valueOf(23));
+			Project _project = (Project) session.get(Project.class,
+					Long.valueOf(11), new LockOptions(LockMode.OPTIMISTIC));
 			_project.setCustomer("New Name For _Project ");
 			project.setCustomer("New Name For Project");
 			session.merge(project);
@@ -58,5 +62,37 @@ public class DaoTesting {
 			session.close();
 		}
 
+	}
+
+	@Test
+	public void testGetAndLoad() {
+		Session session = sessionFactory.openSession();
+		Project project = (Project) session.load(Project.class,
+				Long.valueOf(12), new LockOptions(LockMode.OPTIMISTIC));
+		Transaction t = session.beginTransaction();
+		project.setCustomer("Michael Jackson");
+		session.persist(project);
+		t.commit();
+		Project _project = (Project) session.get(Project.class,
+				Long.valueOf(12), new LockOptions(LockMode.OPTIMISTIC));
+		assertEquals(_project.getCustomer(), "Michael Jackson");
+		session.close();
+	}
+
+	@Test
+	public void testGetSameEntityWithSameId() {
+		Session session = sessionFactory.openSession();
+		Transaction t = session.beginTransaction();
+		Project project = (Project) session
+				.get(Project.class, Long.valueOf(13));
+		Project anotherproject = (Project) session.get(Project.class,
+				Long.valueOf(13));
+		Project _project = (Project) session.get(Project.class,
+				Long.valueOf(13));
+		project.setCustomer("Michael Jackson");
+		_project.setCustomer("DEDO");
+		anotherproject.setCustomer("REWAN");
+		t.commit();
+		session.close();
 	}
 }
